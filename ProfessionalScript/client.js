6d1645faaf42dc47454a46963e855f8e78c8b94f
庞伟力 <pangwilllee@hotmail.com>
2016-02-09 11:53:24 +0800

    =客户端浏览器

---
+++ ProfessionalScript/client.js
@@ -0,0 +1,9 @@
+function isSortable(object) {
+	return typeof object.sort == "function";
+}
+
+function isHostMethod(object, property) {
+	var t = typeof object[property];
+	return t == "function" || (!!(t == "object" && object[property])) || t == "unknown";
+}
+
