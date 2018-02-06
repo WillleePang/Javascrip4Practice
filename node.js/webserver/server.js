524fd9101dd1cd0906738a644e21da2b42ede731
庞伟力 <pangwilllee@hotmail.com>
2018-02-06 22:47:12 +0800

    增加了webserver的启动文件
    如题

--- node.js/webserver/server.js
+++ node.js/webserver/server.js
@@ -1,6 +1,17 @@
 var http = require("http");
-http.createServer(function(request,response){
-	response.writeHead(200,{"Content-type":"text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(8080);
+var url = require("url");
+
+function start(){
+	function onRequest(request,response){
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for "+pathname+" received!");
+		response.writeHead(200,{"Content-type":"text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
+
