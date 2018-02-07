bbb399d31c4fc92c0021cefabdbb8fff431bf5a9
willleepang <pangwilllee@hotmail.com>
2018-02-07 20:23:51 +0800

    完成了node的实例webserver
    如题

--- node.js/webserver/server.js
+++ node.js/webserver/server.js
@@ -1,14 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start(){
+function start(route,handle){
+
 	function onRequest(request,response){
+    	var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for "+pathname+" received!");
-		response.writeHead(200,{"Content-type":"text/plain"});
-		response.write("Hello World");
-		response.end();
-	}
+		route(handle, pathname, response,request);
+  	}
+
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
 }
